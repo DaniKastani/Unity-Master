3eeff56ed50d3f41b7fb480c4d7b879c466a73c4
Daniela Köstenbaumer <koestenbaumer.daniela@gmail.com>
2024-06-10 14:05:13 +0200

    fixing compass

--- Unity-Master/Assets/Scripts/followPlayer.cs
+++ Unity-Master/Assets/Scripts/followPlayer.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class followPlayer : MonoBehaviour
 {
        public Transform player;
+       private Vector3 offset = new Vector3(0f, 0f, -10f);
+       private float smoothTime = 0.25f;
+       private Vector3 velocity = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,8 @@ public class followPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     transform.position = new Vector3(player.transform.position.x, 1.7f , player.transform.position.z);
+       Vector3 targetPosition = new Vector3(player.transform.position.x, player.transform.position.y + 0.15f , player.transform.position.z);
+       transform. position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         
     }
 }
